fcde41a3a528f21c1917e505ea274520330f6824
anchalBomjan <anchal.percoidit@gmail.com>
2024-11-13 15:08:32 +0545

    injecting policy of  product infastructure in Presentation

--- DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
+++ DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,31 @@ using System.Threading.Tasks;
 
 namespace ProductApi.Infrastructure.DependencyInjection
 {
-    internal class ServiceContainer
+    public static  class ServiceContainer
     {
+         public static IServiceCollection AddInfrastructureService(this IServiceCollection services,IConfiguration config)
+        {
+            //Adding databse connectivity
+            //Addinmg authentication scheme
+            SharedServicesContainer.AddSharedServices<ProductDbContext>(services, config, config["MySerilog:FineName"]!);
+
+            //Create Dependency injection ((DI)
+            services.AddScoped<IProduct, ProductRepository>();
+
+            return services;
+
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register middleware such as:
+            //Global Exception:handles  external errors
+            //Listen  to Only API GateWay: Blocks all  outersider calls
+            SharedServicesContainer.UserSharedPolices(app);
+            return app;
+
+
+        
+        }
     }
 }
