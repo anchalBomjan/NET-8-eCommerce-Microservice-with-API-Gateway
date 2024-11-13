e284c21d66d5aba8ff8193b5182885e01e1135d0
anchalBomjan <anchal.percoidit@gmail.com>
2024-11-13 11:14:20 +0545

    adding ProsuctConversion.cs

--- DemoECommerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversions.cs
+++ DemoECommerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductApi.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,43 @@ using System.Threading.Tasks;
 
 namespace ProductApi.Application.DTOs.Conversions
 {
-    public class ProductConversions
+    public  static class ProductConversions
     {
+
+        //entity we pass in this product dto that we created as product and  here we are going to use 
+        public static Product ToEntity(ProductDTO product) => new()
+        {
+            Id= product.Id,
+            Name= product.Name,
+            Quantity=product.Quantity,
+            Price=product.Price
+
+        };
+        // we have  this return type so it must take in the same as a playload
+        // to make request to database 
+        public static (ProductDTO?,IEnumerable<ProductDTO>?) FromEntity(Product product ,IEnumerable<Product>? products)
+        {
+            //return single
+            if(product is null || products is null)
+            {
+                var singleProduct = new ProductDTO
+                    (product!.Id,
+                    product.Name,
+                    product.Quantity,
+                    product.Price
+                    );
+                return (singleProduct, null);
+            }
+            // return list
+             if (products is not null || products is null)
+            {
+                var _products = products!.Select(p =>
+                   new ProductDTO(p.Id, p.Name!, p.Quantity, p.Price)).ToList();
+                return (null,_products);
+            }
+            return (null, null);
+
+        }
 
 
     }
