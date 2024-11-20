6dd99e6dd3568f5fb759dd951677946a7405e3c0
anchalBomjan <anchal.percoidit@gmail.com>
2024-11-20 17:06:02 +0545

    adding middleware in programe.cs and  AttachSignatureToRequest.cs

--- DemoEcommerce.ApiGatewaySolution/ApiGateway.Presentation/Middleware/AttachSignatureToRequest.cs
+++ DemoEcommerce.ApiGatewaySolution/ApiGateway.Presentation/Middleware/AttachSignatureToRequest.cs
@@ -2,5 +2,22 @@
 {
     public class AttachSignatureToRequest
     {
+        private readonly RequestDelegate _next;
+
+        // Constructor accepting RequestDelegate
+        public AttachSignatureToRequest(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        // Middleware logic
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Add custom header to the request
+            context.Request.Headers["Api-Gateway"] = "Signed";
+
+            // Call the next middleware in the pipeline
+            await _next(context);
+        }
     }
 }
